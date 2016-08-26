3b3060c32075038f31ab051a921cd3651f89bc0d
gregory.sng <purepker0@gmail.com>
2016-08-26 14:18:37 +0800

    First Commit

---
+++ CitiZenWeb/src/citi/zen/web/ExampleApplication.java
@@ -0,0 +1,30 @@
+package citi.zen.web;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class ExampleApplication extends Application {
+
+	private Set<Object> singletons = new HashSet<Object>();
+	private Set<Class<?>> empty = new HashSet<Class<?>>();
+	
+	public ExampleApplication(){
+	     singletons.add(new ProductResource());
+	     //singletons.add(new ProductResourceFull());
+	}
+	
+	@Override
+	public Set<Class<?>> getClasses() {
+	     return empty;
+	}
+	
+	@Override
+	public Set<Object> getSingletons() {
+	     return singletons;
+	}
+
+}
---
+++ CitiZenWeb/src/citi/zen/web/ProductResource.java
@@ -0,0 +1,5 @@
+package citi.zen.web;
+
+public class ProductResource {
+
+}
